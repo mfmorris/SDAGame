5392584cd8affd6d21edced62b1658db4b30ac07
jfjones <jfjones@mtech.edu>
2013-12-16 21:22:11 -0700

    heal action

---
+++ SDAGame/HealAction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDAGame
+{
+    /// <summary>
+    /// Action that represents a healing spell. Restores a bit of health
+    /// to the recipient.
+    /// </summary>
+    public class CureLightAction : Action
+    {
+        private Random random;
+
+        public CureLightAction(Random random)
+            : base(1)
+        {
+            this.random = random;
+            this.Name = "Cure Light Wounds";
+            this.Description = "Heals one ally 5 to 10 hp";
+        }
+
+        public override void Resolve(Actor actor, Actor[] targets)
+        {
+            Actor target = targets[0];
+            target.Heal(random.Next(6) + 5);
+        }
+    }
+}
